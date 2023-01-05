e472dad8013516ee91ce165d31acb889aceac46e
sky0815a <sky0815a@gmail.com>
2023-01-04 21:35:23 -0500

    Few more item option tag added

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,6 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const mongoURI = 'mongodb+srv://sky0815a:01172737Aa@cluster0.ptwvtbg.mongodb.net/?retryWrites=true&w=majority'
-  // process.env.NODE_ENV === `production`
-  // ? process.env.DATABASE_URL
-  // :`mongodb://localhost/trackapp`;
 
 mongoose
   .connect(mongoURI)
--- models/Items.js
+++ models/Items.js
@@ -5,10 +5,16 @@ const Schema = mongoose.Schema;
 
 const ItemsSchema = new Schema(
     {
+        Game: String,
+        PW: String,
         type: String,
         cost: Number,
         STR: Number,
-        DEX: Number
+        DEX: Number,
+        Vital: Number,
+        Energy: Number,
+        Faster_Cast_Rate: Number,
+        All_Skill_LV: Number
     }
 );
 
